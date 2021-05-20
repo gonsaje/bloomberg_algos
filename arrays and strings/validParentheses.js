827b0add223ec36142fdd702f5de19e5da7d7964
gonsaje <song.jaehyun@gmail.com>
2021-05-20 11:43:26 -0400

    validParen

--- arrays and strings/validParentheses.js
+++ arrays and strings/validParentheses.js
@@ -71,3 +71,47 @@ var isValid = function(s) {
        return false;
    }
 };
+
+////// SECOND ATTEMPT
+/*
+receive a str of parens
+
+if the length of string is less than 2 return false
+
+create a var for matching brackets
+if its an open paren then add to arr
+if close paren, check if last bracket on the stack is a matching pair and pop off th stack
+otherwise return false
+
+check if stack is empty => all pairs are accounted for
+
+return true
+*/
+// O(n) time and space
+
+var isValid = function(s) {
+    if (s.length < 2) return false;
+    
+    let closed = {
+        '}': '{',
+        ')': '(',
+        ']': '['
+    };
+    
+    let stack = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        let last = stack[stack.length - 1]; // most recent bracket
+        let current = [i];
+        
+        // if the current char is an opening bracket, add to stack
+        if ("({[".includes(current)) stack.push(current); 
+        // if the current is a closing bracket and the last bracket on the stack is the matching pair remove it from stack
+        else if (closed[current] && closed[current] === last) stack.pop();
+        // if current is a closing bracket and there is nothing in the stack OR the last doesnt match the pair of current
+        else if ((closed[current] && stack.length === 0) || (last !== closed[current])) return false; 
+    }
+    
+    if (stack.length === 0) return true; // if all pair matches have been accounted for
+    return false;
+};
